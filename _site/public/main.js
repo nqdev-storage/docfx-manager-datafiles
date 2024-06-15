d2d9c55dfdbd2e7b2a26f06c4d28ab6f01ff1a73
Quy, Nguyen Huu (Mr.) <nguyenquy.1096@gmail.com>
2024-06-15 12:27:00 +0700

    upp

--- _site/public/main.js
+++ _site/public/main.js
@@ -1,1 +1,14 @@
-export default {}
+/**
+ * Licensed to the .NET Foundation under one or more agreements.
+ * The .NET Foundation licenses this file to you under the MIT license.
+ */
+
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/dotnet/docfx',
+      title: 'GitHub'
+    }
+  ]
+}
